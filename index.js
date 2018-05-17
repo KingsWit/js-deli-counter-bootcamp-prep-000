e2236189b47455d63fd4a8691d6af796af4c1cc2
Taylor Tellman <taylortellman@gmail.com>
2018-05-17 16:54:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 function takeANumber(katzDeli, newCustomer){
   katzDeli.push(newCustomer);
   return 'Welcome, '+newCustomer+'. You are number '+katzDeli.length+' in line.';
-  
-}
+  }
+  function nowServing(katzDeli){
+    katzDeli.
+  }
