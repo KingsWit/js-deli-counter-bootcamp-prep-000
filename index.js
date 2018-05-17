1665362e368828d81bfaaf5d9e1bec2412e0e892
Taylor Tellman <taylortellman@gmail.com>
2018-05-17 17:23:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ var line =[];
 function currentLine(katzDeli){
     let i = 0;
     while (i<katzDeli.length){
-     line.push(` `+[i+1]) ;
+     line.push(` `+[i+1]+` `) ;
     }
 if (katzDeli.length ===0){
   return "The line is currently empty."
