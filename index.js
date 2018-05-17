a98926a795967a6b3eb0f7b2b95d3977a6bf3db2
Taylor Tellman <taylortellman@gmail.com>
2018-05-17 17:00:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ if (katzDeli.length === 0){
   return "There is nobody waiting to be served!"
 }
 else {
-  return (`Currently serving ${katzDeli.shift())
+  return (`Currently serving ${katzDeli.shift()}`)
 }
   }
